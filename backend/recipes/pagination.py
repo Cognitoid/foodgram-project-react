eeb01e0c86a7a39bae399f7d9e806f4e64f9b6dc
Cognitoid <enior@list.ru>
2022-09-05 15:15:20 +0300

    Fixed errors in backend-interface communication

---
+++ backend/recipes/pagination.py
@@ -0,0 +1,8 @@
+from rest_framework.pagination import PageNumberPagination
+
+PAGE_SIZE = 6
+
+
+class LimitPageNumberPagination(PageNumberPagination):
+    page_size_query_param = 'limit'
+    page_size = PAGE_SIZE
